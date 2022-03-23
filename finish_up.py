23e89c214bb1139d244e2d1c66d9582f6196077c
Connor Briggs <cgbriggs99@gmail.com>
2022-03-22 21:40:22 -0400

    Should fix it.

--- finish_up.py
+++ finish_up.py
@@ -7,9 +7,12 @@ import shutil
 
 build_dirs = os.listdir("build")
 lib_dir = next(filter(lambda x: re.match("lib\\..*", x), build_dirs))
-files = os.listdir("build/" + lib_dir)
-modfile = next(filter(lambda x: re.match("fastc\\..*\\..*", x), files))
+files = os.listdir("build/" + lib_dir + "/ising")
+try :
+    modfile = next(filter(lambda x: re.match("fastc\\..*\\..*", x), files))
+except :
+    raise Exception(str(files))
 parts = modfile.split(".")
-shutil.copyfile("build/" + lib_dir + "/" + modfile, "ising/" + parts[0] + "."
+shutil.copyfile("build/" + lib_dir + "/ising/" + modfile, "ising/" + parts[0] + "."
                 + parts[-1])
-shutil.copyfile("build/" + lib_dir + "/" + modfile, "ising/" + modfile)
+shutil.copyfile("build/" + lib_dir + "/ising/" + modfile, "ising/" + modfile)
